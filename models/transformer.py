92054128f4367098d423b2b0a2759bec831e4a5f
hoang1645 <hoang.208.1974@gmail.com>
2023-04-08 13:25:26 +0700

    fix the greedy decoder function, remove the legacy mask creation

--- models/transformer.py
+++ models/transformer.py
@@ -13,13 +13,14 @@ class CustomGraformer(nn.Module):
         """
         Implementation of Graformer, as per the article
         "Multilingual Translation via Grafting Pre-trained Language Models", Zewei et al.,
-        Findings of the Association for Computational Linguistics: EMNLP 2021, pages 2735–2747, November 7–11, 2021
+        Findings of the Association for Computational Linguistics: EMNLP 2021, pages 2735-2747, November 7-11, 2021
 
         Parameters:
         :param masked_encoder: the masked LM encoder, mBERT in the original paper. A string for the name of the model
             as per Hugging Face's naming rules, or the model inherited by the Hugging Face's PreTrainedModel.
         :param causal_decoder: the causal decoder, mGPT in the original paper. A string for the name of the model
             as per Hugging Face's naming rules, or the model inherited by the Hugging Face's PreTrainedModel.
+        The two models should be of the same size.        
 
         For the additional K-layer encoder-decoder stack:
         :param d_model: dimension of the model, an int
@@ -27,6 +28,8 @@ class CustomGraformer(nn.Module):
         :param dff: dimension of the feed-forward network
         :param n_encoder_layers: number of encoder layers
         :param n_decoder_layers: number of decoder layers
+
+        d_model, n_heads and dff has to match the masked encoder and causal decoder.
 
         General parameters for the additional stacks:
         :param layer_norm: Layer normalization hyperparameter
@@ -54,14 +57,14 @@ class CustomGraformer(nn.Module):
         self.causal_decoder.requires_grad_(False)
 
         self.k_layer_encoder_stack = nn.TransformerEncoder(
-            nn.TransformerEncoderLayer(d_model, n_heads, dff, dropout, activation, layer_norm_eps=layer_norm),
+            nn.TransformerEncoderLayer(d_model, n_heads, dff, dropout, activation, layer_norm_eps=layer_norm, batch_first=True),
             # norm = nn.LayerNorm(self.masked_encoder.config.vocab_size, layer_norm),
-            num_layers=6#n_encoder_layers
+            num_layers=n_encoder_layers
         )
 
         self.k_layer_decoder_stack = nn.TransformerDecoder(
-            nn.TransformerDecoderLayer(d_model, n_heads, dff, dropout, activation, layer_norm_eps=layer_norm),
-            norm=nn.LayerNorm(self.causal_decoder.config.vocab_size, layer_norm),
+            nn.TransformerDecoderLayer(d_model, n_heads, dff, dropout, activation, layer_norm_eps=layer_norm, batch_first=True),
+            # norm=nn.LayerNorm(self.causal_decoder.config.vocab_size, layer_norm),
             num_layers=n_decoder_layers
         )
 
@@ -70,9 +73,35 @@ class CustomGraformer(nn.Module):
         # HuggingFace will raise errors itself if both tokenizer and model are None.
         self.encoder_tokenizer = AutoTokenizer.from_pretrained(masked_encoder) \
             if encoder_tokenizer is None else encoder_tokenizer
+        
+
+        # Defining special tokens if missing
+        if self.encoder_tokenizer.bos_token_id is None:
+            self.encoder_tokenizer.bos_token_id = self.encoder_tokenizer.unk_token_id
+            self.encoder_tokenizer.bos_token = self.encoder_tokenizer.unk_token
+
+        if self.encoder_tokenizer.eos_token_id is None:
+            self.encoder_tokenizer.eos_token_id = self.encoder_tokenizer.unk_token_id
+            self.encoder_tokenizer.eos_token = self.encoder_tokenizer.unk_token
+
+        if self.encoder_tokenizer.pad_token_id is None:
+            self.encoder_tokenizer.pad_token_id = self.encoder_tokenizer.eos_token_id
+            self.encoder_tokenizer.pad_token = self.encoder_tokenizer.eos_token
+
         self.decoder_tokenizer = AutoTokenizer.from_pretrained(causal_decoder) \
             if decoder_tokenizer is None else decoder_tokenizer
 
+        if self.decoder_tokenizer.bos_token_id is None:
+            self.decoder_tokenizer.bos_token_id = self.decoder_tokenizer.unk_token_id
+            self.decoder_tokenizer.bos_token = self.decoder_tokenizer.unk_token
+
+        if self.decoder_tokenizer.eos_token_id is None:
+            self.decoder_tokenizer.eos_token_id = self.decoder_tokenizer.unk_token_id
+            self.decoder_tokenizer.eos_token = self.decoder_tokenizer.unk_token
+
+        if self.decoder_tokenizer.pad_token_id is None:
+            self.decoder_tokenizer.pad_token_id = self.decoder_tokenizer.eos_token_id
+            self.decoder_tokenizer.pad_token = self.decoder_tokenizer.eos_token
     def forward(self, source, src_mask, target, tgt_mask):
         #TODO: fix this shit to migrate the code to collate_fn
         # encoder_tokens = self.encoder_tokenizer(source, return_tensors='pt', padding='max_length', max_length=128)
@@ -96,14 +125,7 @@ class CustomGraformer(nn.Module):
 
     def decode(self, tgt, memory, tgt_mask):
         causal_decoder_output = self.causal_decoder(tgt, tgt_mask).last_hidden_state
-        return self.k_layer_decoder_stack.forward(causal_decoder_output, memory)
-
-    @staticmethod
-    def generate_square_subsequent_mask(sz, device='cpu'):
-        DEVICE=device
-        mask = (torch.triu(torch.ones((sz, sz), device=DEVICE)) == 1).transpose(0, 1)
-        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
-        return mask
+        return causal_decoder_output, self.k_layer_decoder_stack.forward(causal_decoder_output, memory)
 
     # @staticmethod
     # def create_mask(src, tgt, device='cpu'):
@@ -122,28 +144,32 @@ class CustomGraformer(nn.Module):
         src = src.to(DEVICE)
         src_mask = src_mask.to(DEVICE)
 
+        self.eval()
+
         memory = self.encode(src, src_mask)
+        
         ys = torch.ones(1, 1).fill_(start_symbol).type(torch.long).to(DEVICE)
         for _ in range(max_len-1):
+            
             memory = memory.to(DEVICE)
-            tgt_mask = (__class__.generate_square_subsequent_mask(ys.size(0))
-                        .type(torch.bool)).to(DEVICE)
-            out = self.decode(ys, memory, tgt_mask)
-            out = out.transpose(0, 1)
-            prob = self.lmhead(out[:, -1])
+            tgt_mask = torch.ones(ys.shape)
+            causal_out, out = self.decode(ys, memory, tgt_mask)
+            
+            # out = out.transpose(0, 1)
+            prob = F.softmax(self.lmhead(causal_out[:,-1] + out[:, -1]))
             _, next_word = torch.max(prob, dim=1)
             next_word = next_word.item()
 
             ys = torch.cat([ys,
-                            torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=0)
+                            torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
             if next_word == self.decoder_tokenizer.eos_token_id:
                 break
         return ys
     
     def translate(self, sentence:str):
         self.eval()
-        encoder_tokens = self.encoder_tokenizer(sentence)
+        encoder_tokens = self.encoder_tokenizer(sentence, return_tensors='pt')
         
         encoder_input_ids, encoder_attention_mask = encoder_tokens['input_ids'], encoder_tokens['attention_mask']
-        target_tokens = self.greedy_decode(encoder_input_ids, encoder_attention_mask, max_len=50, start_symbol=self.decoder_tokenizer.pad_token_id)
+        target_tokens = self.greedy_decode(encoder_input_ids, encoder_attention_mask, max_len=50, start_symbol=self.decoder_tokenizer.eos_token_id)
         return self.decoder_tokenizer.batch_decode(target_tokens, skip_special_tokens=True)
