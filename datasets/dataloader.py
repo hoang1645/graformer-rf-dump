5d35f97b2dccdd8094d61fc82b4d76703d63a4e7
hoang1645 <hoang.208.1974@gmail.com>
2023-04-23 15:31:50 +0700

    remove unnecessary params

--- datasets/dataloader.py
+++ datasets/dataloader.py
@@ -7,8 +7,8 @@ def get_dataloader(source, target, src_tokenizer, tgt_tokenizer, batch_size, num
     def collate_fn(batch):
         # print(batch)
         _source, _target = [b[0] for b in batch], [b[1] for b in batch]
-        _source, _target = src_tokenizer(_source, return_tensors='pt', padding=True, pad_left=True), \
-                        tgt_tokenizer(_target, return_tensors='pt', padding=True, pad_left=True)
+        _source, _target = src_tokenizer(_source, return_tensors='pt', padding=True), \
+                        tgt_tokenizer(_target, return_tensors='pt', padding=True)
         return _source, _target
     def test_collate_fn(batch): return [b[0] for b in batch], [b[1] for b in batch]
     return DataLoader(
