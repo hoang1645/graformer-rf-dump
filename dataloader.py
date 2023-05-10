7b9ec747d8a0a855ce4a4bab411278b2afeadfeb
hoang1645 <hoang.208.1974@gmail.com>
2023-05-10 17:18:56 +0700

    truncate the input in case it is too long

--- dataloader.py
+++ dataloader.py
@@ -1,14 +1,16 @@
 from torch.utils.data import DataLoader
 from IWSLTDataset import IWSLTDataset
+from transformers import PreTrainedTokenizer
 
 
-def get_dataloader(source, target, src_tokenizer, tgt_tokenizer, batch_size, num_workers=1, test=False):
+def get_dataloader(source, target, src_tokenizer:PreTrainedTokenizer, tgt_tokenizer:PreTrainedTokenizer, 
+                   batch_size, num_workers=1, test=False):
     # Both tokenizers should have a __call__ method which is equiv. to `convert_tokens_to_ids(tokenize(x))`
     def collate_fn(batch):
         # print(batch)
         _source, _target = [b[0] for b in batch], [b[1] for b in batch]
-        _source, _target = src_tokenizer(_source, return_tensors='pt', padding=True), \
-                        tgt_tokenizer(_target, return_tensors='pt', padding=True)
+        _source, _target = src_tokenizer(_source, return_tensors='pt', padding=True, truncation='longest_first'), \
+                        tgt_tokenizer(_target, return_tensors='pt', padding=True, truncation='longest_first')
         return _source, _target
     def test_collate_fn(batch): return [b[0] for b in batch], [b[1] for b in batch]
     return DataLoader(
